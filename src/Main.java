7b6895ef9e015cdb4bff46ece2064b903753bf66
Rahmatjon <rahmatjon416@gmail.com>
2023-01-01 11:39:57 +0500

    Implement function calc

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,19 @@
 public class Main {
-    public static void main(String[] args) {}
-    public static void calc(String input) throws Exception{}
+    public static void main(String[] args) {
+        try {
+            String input = "";
+            calc(input);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void calc(String input) throws Exception{
+        Expression expression = ExpressionValidator.validateExpressionFromInput(input);
+        if (expression.type == ExpressionType.SIMPLE_NUMBER) {
+            SimpleNumberCalculator.calculate(expression.firstNumber, expression.secondNumber, expression.action);
+        } else {
+            RomanNumberCalculator.calculate(expression.firstNumber, expression.secondNumber, expression.action);
+        }
+    }
 }
